0ec8eed929af58ef1766ce9ca04f0ab735d29492
Wilbert Oliver Chan Uc <olitouc@gmail.com>
2026-09-03 10:25:35 -0600

    cambios corregido
    se corrigio los cambios para una mejor organizacion del sistema , y refactorizacion

--- README.md
+++
@@ -1,2 +0,0 @@
-# sistema-agencia
-sistema agencia 
--- src/server.js
+++
@@ -1,8 +0,0 @@
-//cargaamos el .env
-require("dotenv").config();
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
-});
